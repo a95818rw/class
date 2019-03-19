c336daf6a214c4818b05f7c4c4ed5b4e9536fc01
Shen <gpdgpa@gmail.com>
2019-03-19 14:10:39 +0800

    add snake.png and complete mainactivity layout, add click func on button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    int size = 30;
+
+    public void Click(View view) {
+
+        TextView text = findViewById(R.id.textView);
+        text.setTextSize(++size);
+
+
     }
 }
